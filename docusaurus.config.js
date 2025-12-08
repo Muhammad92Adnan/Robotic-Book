ab3c4ca0500d7fa363e16b496d3989e782d51352
Muhammad_Adnan <adnan92muhammad@gmail.com>
2025-12-09 03:01:09 +0500

    Fix baseUrl for Vercel deployment and update URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://muhammad92adnan.github.io',
+  url: 'https://robotic-book-plum.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Robotic-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
